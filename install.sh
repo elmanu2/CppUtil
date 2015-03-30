38d783de739b426ae2c580156c22e7a7b0d2f01b
omanu <djabolo@gmail.com>
2015-03-30 11:48:52 +0200

    build from comand line

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-mkdir -p bin
+
+MSBUILD=C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe
+
+$MSBUILD CppUtil/Cpputil.vcxproj //t:Clean
+$MSBUILD TestCppUtil/TestCppUtil.vcxproj	//t:Clean
+rm -r bin
+mkdir bin
+
+$MSBUILD CppUtil/Cpputil.vcxproj //p:Configuration=Debug
+$MSBUILD TestCppUtil/TestCppUtil.vcxproj //p:Configuration=Debug
+
+$MSBUILD CppUtil/Cpputil.vcxproj //p:Configuration=Release
+$MSBUILD TestCppUtil/TestCppUtil.vcxproj //p:Configuration=Release
 cp CppUtil/bin/*.dll bin/
 cp TestCppUtil/bin/*.exe bin/
