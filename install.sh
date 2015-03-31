7e8a1d7925d3c6605ba170e1a803cc29f72f3052
omanu <djabolo@gmail.com>
2015-03-31 11:47:49 +0200

    split install and clean script

--- install.sh
+++ install.sh
@@ -2,10 +2,7 @@
 
 MSBUILD=C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe
 
-$MSBUILD CppUtil/Cpputil.vcxproj //t:Clean
-$MSBUILD TestCppUtil/TestCppUtil.vcxproj	//t:Clean
-rm -r bin
-mkdir bin
+mkdir -p bin
 
 $MSBUILD CppUtil/Cpputil.vcxproj //p:Configuration=Debug
 $MSBUILD TestCppUtil/TestCppUtil.vcxproj //p:Configuration=Debug
